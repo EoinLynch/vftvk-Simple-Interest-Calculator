4626f8460260f3208a14fd16d33b642b999f63b6
Eoin Lynch <el@group-treetop.lu>
2022-01-08 03:19:51 +0100

    done

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    principal = document.getElementById("principal").value;
+	rate = document.getElementById("rateInput").value;
+	years = document.getElementById("yearSelection").value;
+    interest  = principal*rate*years/100;
+	output = document.getElementById("outputValue");
+    endYear = new Date().getFullYear()+parseInt(years);
+    output.innerText= "If you deposit " + principal + ",\nat an interest rate of " + rate + "%,\nYou will receive an amount of " + Math.round(interest) +",\nin the year " + endYear +"\n";
+
 }
+
+function updateSliderText(val) {
+       var output = document.getElementById("sliderSelectedValue");
+       var input = document.getElementById("rateInput");
+       output.innerText = input.value + "%";
+}
+        
         
